c6ceab6778d130d9ad6aa65261bb9b437097b537
bornwarrior <jpatel@localhost.localdomain>
2017-10-02 21:20:18 -0400

    Fixed build for Fedora

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rf _builds _steps _projects _cache _temp
-wercker build --git-domain github.com    --git-owner bornwarrior   --git-repository Chp3TeamService
+wercker build --docker-host "unix:///var/run/docker.sock" --git-domain github.com    --git-owner bornwarrior   --git-repository Chp3TeamService
 rm -rf _builds _steps _projects _cache _temp
 
